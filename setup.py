b942d48ede70dca65528153635a92af4a23e13e7
anastasiia3006 <111446004+Hornostai@users.noreply.github.com>
2022-12-08 10:31:59 +0100

    upgreat10.0

--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
 from setuptools import setup, find_namespace_packages
 #інструкції для встановлення проекту
 setup(
-   name='clean_folder_anastasia',
-    version='0.1.0',
+    name='clean_folder_anastasia',
+    version='0.1.1',
     description='Hw 7. Clean folder',
-    author='Name',
+    author='A.G.',
     author_email='gornanastasia3006@gmail.com',
     license='MIT',
-)
-
-classifiers=[
-        "Python  version :: Pzthon :: 3",
-        "Licence :: OSI Approved :: MIT Licence",
-        "Operatin Sistem :: OS Independent",
-    ],
-packages=find_namespace_packages("src")
-entry_points={'console_scripts': [
-        'clean=clean_folder.clean:main']}
+    packages=find_namespace_packages(),
+    entry_points={'console_scripts': [
+        'clean=clean_folder.clean:main']})
